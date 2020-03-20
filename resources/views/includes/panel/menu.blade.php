feb229a10a1ff6f02c7d92e91e7229a86c759a81
Abel <ruffo_roque@hotmail.com>
2020-03-20 01:04:32 -0500

    Base Project

---
+++ resources/views/includes/panel/menu.blade.php
@@ -0,0 +1,61 @@
+<!-- Nav items -->
+<!-- Heading -->
+<h6 class="navbar-heading p-0 text-muted">
+    <span class="docs-normal">Gestionar Datos</span>
+</h6>
+<ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="nav-link active" href="/">
+            <i class="ni ni-tv-2 text-danger"></i>
+            <span class="nav-link-text">Dashboard</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="examples/icons.html">
+            <i class="ni ni-planet text-blue"></i>
+            <span class="nav-link-text">Especialidades</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="examples/map.html">
+            <i class="ni ni-single-02 text-orange"></i>
+            <span class="nav-link-text">Médicos</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="examples/profile.html">
+            <i class="ni ni-satisfied text-info"></i>
+            <span class="nav-link-text">Pacientes</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="" onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
+            <i class="ni ni-key-25"></i>
+            <span class="nav-link-text">Cerrar Sesión</span>
+        </a>
+        <form action="{{ route('logout') }}" method="POST" style="display: none" id="formLogout">
+            @csrf
+        </form>
+    </li>
+</ul>
+<!-- Divider -->
+<hr class="my-3">
+<!-- Heading -->
+<h6 class="navbar-heading p-0 text-muted">
+    <span class="docs-normal">Reportes</span>
+</h6>
+<!-- Navigation -->
+<ul class="navbar-nav mb-md-3">
+    <li class="nav-item">
+        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html" target="_blank">
+            <i class="ni ni-sound-wave text-yellow"></i>
+            <span class="nav-link-text">Frecuencia de Citas</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html" target="_blank">
+            <i class="ni ni-spaceship text-orange"></i>
+            <span class="nav-link-text">Médicos más activos</span>
+        </a>
+    </li>
+</ul>
---
+++ resources/views/layouts/form.blade.php
@@ -0,0 +1,135 @@
+<!--
+=========================================================
+* Argon Dashboard - v1.2.0
+=========================================================
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+
+* Copyright  Creative Tim (http://www.creative-tim.com)
+* Coded by www.creative-tim.com
+=========================================================
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    <title>{{ config('app.name') }} | @yield('title')</title>
+    <!-- Favicon -->
+    <link rel="icon" href=" {{ asset('img/brand/favicon.png') }}" type="image/png">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{ asset('vendor/nucleo/css/nucleo.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
+    <!-- Argon CSS -->
+    <link rel="stylesheet" href="{{ asset('css/argon.css?v=1.2.0') }}" type="text/css">
+</head>
+
+<body class="bg-default">
+<!-- Navbar -->
+<nav id="navbar-main" class="navbar navbar-horizontal navbar-transparent navbar-main navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            <img src="{{ asset('img/brand/white.png') }}">
+{{--            {{ config('app.name') }}--}}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse navbar-custom-collapse collapse" id="navbar-collapse">
+            <div class="navbar-collapse-header">
+                <div class="row">
+                    <div class="col-6 collapse-brand">
+                        <a href="/">
+                            <img src="{{ asset('img/brand/blue.png') }}">
+                        </a>
+                    </div>
+                    <div class="col-6 collapse-close">
+                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse" aria-controls="navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+                            <span></span>
+                            <span></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a href="{{ route('login') }}" class="nav-link">
+                        <span class="nav-link-inner--text">Ingresar</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('register') }}" class="nav-link">
+                        <span class="nav-link-inner--text">Registro</span>
+                    </a>
+                </li>
+            </ul>
+            <hr class="d-lg-none" />
+        </div>
+    </div>
+</nav>
+<!-- Main content -->
+<div class="main-content">
+    <!-- Header -->
+    <div class="header bg-gradient-primary py-6 py-lg-7 pt-lg-8">
+        <div class="container">
+            <div class="header-body text-center mb-7">
+                <div class="row justify-content-center">
+                    <div class="col-xl-5 col-lg-6 col-md-8 px-5">
+                        <h1 class="text-white">
+                            @yield('title','Welcome!')
+                        </h1>
+                        <p class="text-lead text-white">
+                            @yield('subtitle','')
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="separator separator-bottom separator-skew zindex-100">
+            <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+            </svg>
+        </div>
+    </div>
+    <!-- Page content -->
+    @yield('content')
+</div>
+<!-- Footer -->
+<footer class="py-5" id="footer-main">
+    <div class="container">
+        <div class="row align-items-center justify-content-xl-between">
+            <div class="col-xl-6">
+                <div class="copyright text-center text-xl-left text-muted">
+                    &copy; 2020 <a href="/" class="font-weight-bold ml-1">{{ config('app.name') }}</a>
+                </div>
+            </div>
+            <div class="col-xl-6">
+                <ul class="nav nav-footer justify-content-center justify-content-xl-end">
+                    <li class="nav-item">
+                        <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Acerca de</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Argon Scripts -->
+<!-- Core -->
+<script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('vendor/js-cookie/js.cookie.js') }}"></script>
+<script src="{{ asset('vendor/jquery.scrollbar/jquery.scrollbar.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js') }}"></script>
+<!-- Argon JS -->
+<script src="{{ asset('js/argon.js?v=1.2.0') }}"></script>
+</body>
+
+</html>
